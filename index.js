7b3131ef14ceec1363337b3427afc0f418291cf1
Husain Haekal <mhh3101@gmail.com>
2025-08-03 01:10:30 +0800

    url changed

--- index.js
+++ index.js
@@ -149,8 +149,8 @@ app.post("/create-checkout-session", async (req, res) => {
             payment_method_types: ["card"],
             line_items: lineItems,
             mode: "payment",
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cart",
+            success_url: "https://iron-fuel-frontend-e7xl.vercel.app/success",
+            cancel_url: "https://iron-fuel-frontend-e7xl.vercel.app/cart",
         });
 
         res.json({ url: session.url });
